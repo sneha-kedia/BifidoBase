eb594621169bcd898f639c8705b214e82f3aef94
sneha-kedia <snehabio1211@gmail.com>
2026-04-07 17:28:55 +0530

    Render deployment setup

--- app.py
+++ app.py
@@ -186,5 +186,8 @@ def autocomplete():
 # -------------------------------
 # RUN
 # -------------------------------
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True, port=10000)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
